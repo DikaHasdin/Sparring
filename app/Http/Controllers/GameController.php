a366d82957ab2f3522c20910abc51f24bc03d9fd
DikaHasdin <andikahasanuddin12@gmail.com>
2023-08-01 09:29:01 +0700

    menambah fitur edit dan hapus pada game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,6 +10,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Http\RedirectResponse;
 
+use Illuminate\Support\Facades\Storage;
+
 class GameController extends Controller
 {
     public function index(): View
@@ -38,13 +40,73 @@ class GameController extends Controller
         $image = $request->file('image');
         $image->storeAs('public/games', $image->hashName());
 
-        //create post
+        //create game
         Game::create([
             'nama_game'     => $request->nama_game,
-            'image'     => $image->hashName(),
+            'image'         => $image->hashName(),
         ]);
 
         //redirect to index
         return redirect()->route('games.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+    public function edit(string $id): View
+    {
+        $game = Game::findOrFail($id);
+
+        return view('games.edit', compact('game'));
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        //validate form
+        $this->validate($request, [
+            'nama_game'     => 'required|min:5',
+            'image'     => 'required|image|mimes:jpeg,jpg,png|max:2048',
+        ]);
+
+        //get game by ID
+        $game = Game::findOrFail($id);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/games', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/games/' . $game->image);
+
+            //update game with new image
+            $game->update([
+                'nama_game'     => $request->nama_game,
+                'image'         => $image->hashName(),
+            ]);
+        } else {
+
+            //update game without image
+            $game->update([
+                'nama_game'     => $request->nama_game,
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('games.index')->with(['success' => 'Data Berhasil Diubah!']);
+    }
+
+    public function destroy($id): RedirectResponse
+    {
+        //get game by ID
+        $game = Game::findOrFail($id);
+
+        //delete image
+        Storage::delete('public/games/'. $game->image);
+
+        //delete game
+        $game->delete();
+
+        //redirect to index
+        return redirect()->route('games.index')->with(['success' => 'Data Berhasil Dihapus!']);
     }
 }
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +9,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
+
 <body style="background: rgb(255, 255, 255)">
 
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Data Game</h3>     
+                    <h3 class="text-center my-4">Data Game</h3>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
@@ -22,59 +24,62 @@
                         <a href="{{ route('games.create') }}" class="btn btn-md btn-success mb-3">Tambah game</a>
                         <table class="table table-bordered">
                             <thead>
-                              <tr>
-                                <th scope="col">NAMA GAME</th>
-                                <th scope="col">GAMBAR</th>
-                                <th scope="col">AKSI</th>
-                              </tr>
+                                <tr>
+                                    <th scope="col">NAMA GAME</th>
+                                    <th scope="col">GAMBAR</th>
+                                    <th scope="col">AKSI</th>
+                                </tr>
                             </thead>
                             <tbody>
-                              @forelse ($games as $game)
-                                <tr>
-                                    <td>{{ $game->nama_game }}</td>
-                                    <td class="text-center">
-                                        <img src="{{ asset('/storage/games/'.$game->image) }}" class="rounded" style="width: 100px">
-                                    </td>
-                                    <td class="text-center">
-                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('games.destroy', $game->id) }}" method="POS-T">
-                                            <a href="{{ route('games.show', $game->id) }}" class="btn btn-sm btn-dark">SHOW</a>
-                                            <a href="{{ route('games.edit', $game->id) }}" class="btn btn-sm btn-primary">EDIT</a>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                              @empty
-                                  <div class="alert alert-danger">
-                                      Data game belum Tersedia.
-                                  </div>
-                              @endforelse
+                                @forelse ($games as $game)
+                                    <tr>
+                                        <td>{{ $game->nama_game }}</td>
+                                        <td class="text-center">
+                                            <img src="{{ asset('/storage/games/' . $game->image) }}" class="rounded"
+                                                style="width: 100px">
+                                        </td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');"
+                                                action="{{ route('games.destroy', $game->id) }}" method="POST">
+                                                {{-- <a href="{{ route('games.show', $game->id) }}"
+                                                    class="btn btn-sm btn-dark">SHOW</a> --}}
+                                                <a href="{{ route('games.edit', $game->id) }}"
+                                                    class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data game belum Tersedia.
+                                    </div>
+                                @endforelse
                             </tbody>
-                          </table>  
-                          {{ $games->links() }}
+                        </table>
+                        {{ $games->links() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <script>
         //message with toastr
-        @if(session()->has('success'))
-        
-            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+        @if (session()->has('success'))
 
-        @elseif(session()->has('error'))
+            toastr.success('{{ session('success') }}', 'BERHASIL!');
+        @elseif (session()->has('error'))
 
-            toastr.error('{{ session('error') }}', 'GAGAL!'); 
-            
+            toastr.error('{{ session('error') }}', 'GAGAL!');
         @endif
     </script>
 
 </body>
+
 </html>
