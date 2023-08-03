2098a79c18e02f6f1c87aa25138eafaa200373c3
DikaHasdin <andikahasanuddin12@gmail.com>
2023-08-03 11:51:41 +0700

    Membuat template

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::resource('/games',\App\Http\Controllers\GameController::class);
 Route::resource('/ruangans',\App\Http\Controllers\RuanganController::class);
